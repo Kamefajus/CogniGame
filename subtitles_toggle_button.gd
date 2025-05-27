aa2af9e3275350365ff7524889e0e66ecd8e997a
jannygames <jamolisdev@gmail.com>
2025-05-28 01:58:36 +0300

    Settings updated with colorblindness selections

--- subtitles_toggle_button.gd
+++ subtitles_toggle_button.gd
@@ -16,3 +16,28 @@ func set_label_text(button_pressed : bool) -> void:
 
 func on_subtitles_toggled(button_pressed : bool) -> void:
 	set_label_text(button_pressed)
+
+
+func _on_none_button_pressed() -> void:
+	ColorProfile.mode = "normal"
+	ColorProfile.apply()
+
+
+func _on_prota_button_pressed() -> void:
+	ColorProfile.mode = "protanopia"
+	ColorProfile.apply()
+
+
+func _on_deuter_button_pressed() -> void:
+	ColorProfile.mode = "deuteranopia"
+	ColorProfile.apply()
+
+
+func _on_trita_button_pressed() -> void:
+	ColorProfile.mode = "tritanopia"
+	ColorProfile.apply()
+
+
+func _on_achroma_button_pressed() -> void:
+	ColorProfile.mode = "achromatopsia"
+	ColorProfile.apply()
